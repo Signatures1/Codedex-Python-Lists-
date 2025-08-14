d0d7043775e2a58fcbd9cd265c3a6f9d626dd7a3
Sheng <demonicplayer404@gmail.com>
2025-08-14 14:01:50 -0400

    fourth part

---
+++ reading_list.py
@@ -0,0 +1,10 @@
+books = ['Harry Potter',
+         '1984',
+         'The Fault in Our Stars',
+         'The Mom Test',
+         'Life in Code']
+print(books)
+books.append('Pachinko')
+books.remove('1984')
+books.pop(1)
+print(books)

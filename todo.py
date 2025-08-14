afa56f2004930783d21131afc80cdc1505162bd3
Sheng <demonicplayer404@gmail.com>
2025-08-14 13:04:32 -0400

    second part

---
+++ todo.py
@@ -0,0 +1,12 @@
+todo_list = ['🏦 Get quarters.',
+             '🧺 Do laundry.',
+             '🌳 Take a walk.',
+             '💈 Get a haircut.',
+             '🍵 Make some tea.',
+             '💻 Complete Lists chapter.',
+             '💖 Call mom.',
+             '📺 Watch My Hero Academia.']
+print(todo_list[0])
+print(todo_list[1])
+print(todo_list[2:5])
+#print(todo_list[9])  #checking error

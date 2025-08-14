79214fdeaf706317b62ca520dfe81755db9605f1
Sheng <demonicplayer404@gmail.com>
2025-08-14 13:21:47 -0400

    third part

---
+++ inventory.py
@@ -0,0 +1,3 @@
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+print("The lowest LEGO part are the elves running low on is:", min(lego_parts))
+print("The highest LEGO part are the elves running low on is:", max(lego_parts))
